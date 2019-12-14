a0ca25202777c911a1e7bb5d09944e9a2c153b4d
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 03:10:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie]
 end
 
 def adding_matz
@@ -81,7 +82,11 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash = 
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
 end
 
 def changing_alan
